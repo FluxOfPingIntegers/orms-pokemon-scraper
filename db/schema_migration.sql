f99c590aa4afb4599f58f5875a6d8b8b9dab8404
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-04-06 17:24:56 -0400

    Done.

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,2 +1,3 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
+CREATE TABLE IF NOT EXISTS pokemon (name TEXT, type TEXT, db TEXT, id INTEGER PRIMARY KEY);
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,42 @@
+require 'pry'
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+  
+  def initialize(name:, type:, db:, id:)
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    @db = db
+    sql = <<-SQL
+    SELECT * FROM pokemon 
+    WHERE name = ?
+    AND type = ?
+    SQL
+    response = db.execute(sql, name, type)[0]
+    if response
+        self.find(response[-1])
+    else
+        sql = <<-SQL
+        CREATE TABLE IF NOT EXISTS pokemon (name TEXT, type TEXT, db TEXT, id INTEGER PRIMARY KEY);
+
+        INSERT INTO pokemon (name, type)
+        VALUES (?, ?);
+        SQL
+        response = db.execute(sql, name, type)[0]
+    end
+  end
+
+  def find(id)
+    sql = <<-SQL
+        SELECT * FROM pokemon WHERE id = ?
+        SQL
+    response = @db.execute(sql, id)[0]
+    binding.pry
+
+  end
+
 end
